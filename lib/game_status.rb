14215b96356f0f6aa4a839701bc72790645ee1a5
DJ Jaiswal <digvijays18@gmail.com>
2018-03-20 19:13:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,10 +32,7 @@ def won?(board)
     if position_1=="X" && position_2=="X" && position_3=="X"
        return [win_index_1,win_index_2,win_index_3]
     else
-      #board.each do |element|
-      #  element == ""
-      #end
-        return false
+       false
     end
   end
 end
